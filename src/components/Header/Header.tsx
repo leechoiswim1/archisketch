194db0e53e130436cba47d7c1f6224814439fe65
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-03-20 00:24:47 +0900

    :hammer: 모달 헤더시 모달 닫기 함수 props 전달

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import * as S from './Header.styled';
 import Cancle from 'assets/icons/x.svg';
-const Header = () => {
+import { HeaderProps } from './Header.type';
+const Header = ({ modalCloseHandler }: HeaderProps) => {
   return (
     <S.Header>
-      <S.Button>
+      <S.Button onClick={modalCloseHandler}>
         <img src={Cancle} alt="Cancle Icon" />
       </S.Button>
     </S.Header>
