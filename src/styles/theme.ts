bd402476d44ac44f816295e243b4c22833da2e9c
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-03-19 22:22:08 +0900

    :lipstick:  theme color추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,9 @@
 const colors = {
+  white: '#ffffff',
+  fontGray: '#999999',
+  gray: '#666666',
+  deepGray: '#2d3236',
+  borderGray: '#d9e7e9',
   background: '#FFFEFF',
   borderColor: '#efeeef',
 };
