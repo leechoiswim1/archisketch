0b61ea381e8ab8c9cd34ca76d7341ab3fd570bb3
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-03-19 22:21:50 +0900

    :hammer: 코드 이동, 삭제 및 코드 index로 내보내기

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as Header } from './Header/Header';
 export { default as Gallery } from './Gallery/Gallery';
+export { default as GalleryInfo } from './GalleryInfo/GalleryInfo';
+export { default as Card } from './Card/Card';
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from 'styles/GlobalStyle';
 import theme from 'styles/theme';
-import App from './App';
+import App from 'page/App';
 
 ReactDOM.render(
   <React.StrictMode>
