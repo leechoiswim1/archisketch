76835256a97e02d5df00438b1eb4ba175a068494
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-03-21 02:56:52 +0900

    :bento: icon 추가

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,6 @@
+export { default as arrowLeft } from './arrowLeft.svg';
+export { default as arrowRight } from './arrowRight.svg';
+export { default as download } from './download.svg';
+export { default as tag } from './tag.svg';
+export { default as trash } from './trash.svg';
+export { default as x } from './x.svg';
