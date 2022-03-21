7c19bcca8f489aa689f1aa141c900287e9f10a8e
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-03-22 05:00:19 +0900

    :hammer: 스피너 translate 수정

--- src/components/Spinner/Spinner.styled.ts
+++ src/components/Spinner/Spinner.styled.ts
@@ -6,8 +6,8 @@ export const Wrapper = styled.div`
 
 export const Spin = styled.div`
   position: absolute;
-  width: 30px;
-  height: 30px;
+  width: 60px;
+  height: 60px;
   top: 0;
   bottom: 0;
   left: 0;
@@ -16,31 +16,31 @@ export const Spin = styled.div`
 `;
 
 const dot1 = keyframes`   
-    0%, 100% {transform: translate(0, 0); opacity:1} 
-    25% {transform: translate(0, 15px); opacity:0.5} 
-    50% {transform: translate(15px, 15px);opacity:0.5} 
-    75% {transform: translate(15px, 0);opacity:0.5} 
+    0%, 100% {transform: translate(7.5px, 7.5px); opacity:1} 
+    25% {transform: translate(37.5px, 7.5px); opacity:0.5} 
+    50% {transform: translate(37.5px, 37.5px);opacity:0.5} 
+    75% {transform: translate(7.5px, 37.5px );opacity:0.5} 
 `;
 
 const dot2 = keyframes` 
-    0%, 100% {transform: translate(15px, 0px); opacity:0.5} 
-    25% {transform: translate(0px,0px); opacity:1}  
-    50% {transform: translate(0px,15px);opacity:0.5} 
-    75% {transform: translate(15px, 15px);opacity:0.5} 
+    0%, 100% {transform: translate(37.5px, 7.5px); opacity:0.5} 
+    25% {transform: translate(37.5px, 37.5px); opacity:1}  
+    50% {transform: translate(7.5px, 37.5px);opacity:0.5} 
+    75% {transform: translate(7.5px, 7.5px);opacity:0.5} 
 `;
 
 const dot3 = keyframes`  
-    0%, 100% {transform: translate(15px, 15px);opacity:0.5} 
-    25% {transform: translate(15px,0px ); opacity:0.5}  
-    50% {transform: translate(0, 0);opacity:1} 
-    75% {transform: translate(0px, 15px);opacity:0.5} 
+    0%, 100% {transform: translate(37.5px, 37.5px);opacity:0.5} 
+    25% {transform: translate(7.5px, 37.5px); opacity:0.5}  
+    50% {transform: translate(7.5px, 7.5px);opacity:1} 
+    75% {transform: translate(37.5px, 7.5px);opacity:0.5} 
 `;
 
 const dot4 = keyframes` 
-    0%, 100% {transform: translate(0px, 15px); opacity:0.5} 
-    25% {transform: translate(15px, 15px); opacity:0.5} 
-    50% {transform: translate(15px, 0px); opacity:0.5} 
-    75% {transform: translate(0, 0); opacity:1} 
+    0%, 100% {transform: translate(7.5px, 37.5px); opacity:0.5} 
+    25% {transform: translate(7.5px, 7.5px); opacity:0.5} 
+    50% {transform: translate(37.5px, 7.5px); opacity:0.5} 
+    75% {transform: translate(37.5px, 37.5px); opacity:1} 
 `;
 
 export const Dot = styled.div`
@@ -50,25 +50,15 @@ export const Dot = styled.div`
   position: absolute;
   border-radius: 50%;
   :nth-child(1) {
-    top: 0;
-    left: 0;
-    z-index: 1;
-    animation: ${dot1} 1.8s linear infinite;
+    animation: ${dot1} 10s linear infinite;
   }
   :nth-child(2) {
-    top: 0;
-    right: 0;
-    animation: ${dot2} 1.8s linear infinite;
+    animation: ${dot2} 10s linear infinite;
   }
   :nth-child(3) {
-    bottom: 0;
-    right: 0;
-    z-index: 1;
-    animation: ${dot3} 1.8s linear infinite;
+    animation: ${dot3} 10s linear infinite;
   }
   :nth-child(4) {
-    bottom: 0;
-    left: 0;
-    animation: ${dot4} 1.8s linear infinite;
+    animation: ${dot4} 10s linear infinite;
   }
 `;
