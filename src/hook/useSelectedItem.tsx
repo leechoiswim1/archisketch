5c307b37c38afe9a336bc76097aa1cb4ddb774bb
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-03-22 05:03:29 +0900

    :hammer: 모달에서 이미지 삭제하고 뒤로가기시 이미지 undefined 에러 처리

--- src/hook/useSelectedItem.tsx
+++ src/hook/useSelectedItem.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { ItemsType } from 'components/Gallery/Gallery.type';
 import { CardImage } from 'components/Card/Card.type';
+
 const useSelectedItem = (items: ItemsType) => {
   const [selectedItem, setSelectedItem] = useState<number>(0);
   const [selectedUrl, setSelectedUrl] = useState<string>('');
@@ -18,8 +19,14 @@ const useSelectedItem = (items: ItemsType) => {
   useEffect(() => {
     if (selectedItem && items) {
       const idx = items.findIndex((item: CardImage) => item.id === selectedItem);
-      const imageUrl = items[idx].imageUrl;
-      setSelectedUrl(imageUrl);
+      if (idx === -1) {
+        const idx = items.findIndex((item: CardImage) => item.id === selectedItem - 1);
+        const imageUrl = items[idx].imageUrl;
+        setSelectedUrl(imageUrl);
+      } else {
+        const imageUrl = items[idx].imageUrl;
+        setSelectedUrl(imageUrl);
+      }
     }
   }, [selectedItem, items]);
 
