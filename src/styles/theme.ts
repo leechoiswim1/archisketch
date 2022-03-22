6d39afdde6baf1d5d611baba81cba3f5d3058a22
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-03-23 03:41:24 +0900

    :art: theme정리

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,17 +1,17 @@
 const colors = {
   white: '#ffffff',
-  fontGray: '#999999',
-  selectGray: '#595959',
-  gray: '#666666',
-  deepGray: '#2d3236',
   borderGray: '#d9e7e9',
   background: '#FFFEFF',
   borderColor: '#efeeef',
   buttonColor: '#f3f4f4',
   imageColor: '#fafafa',
-  blue: '#499fb6',
+  fontGray: '#999999',
+  selectGray: '#595959',
+  gray: '#666666',
+  deepGray: '#2d3236',
   backgroundHover: '#4b4f54b3',
   buttonFont: '#4b4b4b',
+  blue: '#499fb6',
 };
 
 const theme = {
