528be50b85d8c5d133ed933232cd9b130c8996e7
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-03-22 23:52:21 +0900

    :hammer: 체크아이템리스트 선택된 체크아이템 삭제 로직 추가

--- src/hook/useAddCheckItem.tsx
+++ src/hook/useAddCheckItem.tsx
@@ -4,7 +4,9 @@ const useAddCheckItem = () => {
   const [checkItems, setCheckItems] = useState<number[]>([]);
 
   const checkItemHandler = (item: number) => {
-    setCheckItems([...checkItems, item]);
+    checkItems.includes(item)
+      ? setCheckItems([...checkItems.filter(checkItem => checkItem !== item)])
+      : setCheckItems([...checkItems, item]);
   };
   return { checkItems: checkItems, checkItemHandler: checkItemHandler };
 };
