4bf8c813cd6a83dc4efabb791d5bcc124cd3bcf4
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-03-22 23:55:42 +0900

    :hammer: api주소 변경

--- src/service/Api.tsx
+++ src/service/Api.tsx
@@ -4,7 +4,7 @@ import { successResponse, errorResponse } from './Api.type';
 export const getData = async () => {
   try {
     const { data } = await request.get<successResponse>(
-      'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/a7804447-abeb-473e-be8b-8025c4f624f6/test.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220321%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220321T141346Z&X-Amz-Expires=86400&X-Amz-Signature=adfa1b1289e94c10e25a5233c228c74874ce3709ed713649e3c2ecba309c93ba&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22test.json%22&x-id=GetObject',
+      'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/a7804447-abeb-473e-be8b-8025c4f624f6/test.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220322%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220322T145449Z&X-Amz-Expires=86400&X-Amz-Signature=d03cbf25ff71c07618da4d97876b0a864d6f5f25b48ab7d8ff735d9fe142e8f7&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22test.json%22&x-id=GetObject',
     );
     return data;
   } catch (err) {
